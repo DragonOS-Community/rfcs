5c4f61f85a33d45e050d8b0c61d9a4defdee91a9
Baiye <17881468910@163.com>
2024-05-20 11:37:05 +0000

    darkmode and  theme color

---
+++ docs/src/.vuepress/config.ts
@@ -0,0 +1,20 @@
+import { defineUserConfig } from "vuepress";
+import theme from "./theme.js";
+import { hopeTheme } from "vuepress-theme-hope";
+
+export default defineUserConfig({
+  base: "/",
+
+  lang: "zh-CN",
+  title: "博客演示！！！",
+  description: "vuepress-theme-hope 的博客演示",
+
+// 和 PWA 一起启用
+// shouldPrefetch: false,
+ 
+  theme: hopeTheme({
+    darkmode: "switch",
+  }),
+//深色模式
+  
+});
